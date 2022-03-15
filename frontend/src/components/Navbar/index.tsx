8d47a113061aea2643691836ed8e83ce2444e0d2
FabTrindade <fabrtrindade@gmail.com>
2022-03-15 19:53:06 -0300

    Github referece changed

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -9,10 +9,10 @@ function Navbar()
         <nav className = "container">
           <div className = "dsmovie-nav-content">
             <h1>DSMovie</h1>
-            <a href= "https://github.com/devsuperior">
+            <a href= "https://github.com/FabTrindade">
               <div className ="dsmovie-contact-container">
                 <GithubIcon/>
-                <p className="dsmovie-contact-link">/devsuperior</p>
+                <p className="dsmovie-contact-link">/FabTrindade</p>
               </div>
             </a>
           </div>
